3ca43694e2a35d24217ee98d2dd3c0c5e11166e2
reyanRaj <72247352+reyanRaj@users.noreply.github.com>
2023-01-26 21:43:55 +0530

    asdasd

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ const controller = require("../controllers/auth");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "x-access-token, Origin, Content-Type, Accept"
+    // );
     next();
   });
 
--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,10 @@ const authJwt = require("../middlewares/authJwt");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "x-access-token, Origin, Content-Type, Accept"
+    // );
     next();
   });
 
